ef948ca36f92ca83be6b65c45211081e51a7df75
AhmedMedo <ahmed.alaa.eldin.hamdy@gmail.com>
2025-12-22 16:53:38 +0200

    update recipies list

--- app/Components/Recipe/Infrastructure/Http/Handler/Recipe/RecipeListHandler.php
+++ app/Components/Recipe/Infrastructure/Http/Handler/Recipe/RecipeListHandler.php
@@ -109,13 +109,13 @@ class RecipeListHandler extends Handler
     public function __invoke(Request $request): \Illuminate\Http\JsonResponse
     {
         $userUuid = null;
-        if ($this->userService->isAuthenticated() && !$request->query('is_favorite') && !$request->query('is_admin')) {
+        if (($this->userService->isAuthenticated() && !$request->query('is_favorite') && !$request->query('is_admin')) || $request->query('full_recipes') ) {
             $user = $this->userService->user();
             $userUuid = $user->uuid();
         }
         $recipes = $this->recipeService->paginated(
             userUuid: $userUuid,
-            withAdminRecipes: true,
+            withAdminRecipes: $request->has('full_recipes') && $request->query('full_recipes'),
         );
         return $this->successResponseWithDataAndMeta(
             data: $recipes->map(fn (RecipeEntity $recipe) => $this->recipeViewModelMapper->fromEntity($recipe)->toArray())->toArray(),
